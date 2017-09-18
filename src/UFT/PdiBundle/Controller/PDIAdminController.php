f09bff7321febc6d7f9e3c36a7df760cab80e108
Elencarlos Soares <elencarlos@uft.edu.br>
2017-09-18 11:04:35 -0300

    criacao das entidades

--- src/UFT/PdiBundle/Controller/PDIAdminController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UFT\PdiBundle\Controller;
-
-use Sonata\AdminBundle\Controller\CRUDController;
-
-class PDIAdminController extends CRUDController
-{
-}
--- src/UFT/PdiBundle/PdiBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UFT\PdiBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class PdiBundle extends Bundle
-{
-}
